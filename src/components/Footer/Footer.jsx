e443de4a688f4ee99be767f25e7db64c403d2474
Avriel <tractar313@gmail.com>
2023-05-02 18:28:32 +0600

    formate in folder Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css'
+import lowersvg from './Vector lower.svg';
+
+const Footer = () => {
+    return(
+        <footer>
+            <img src={lowersvg}/>
+        </footer>
+    )
+}
+
+export default Footer;
