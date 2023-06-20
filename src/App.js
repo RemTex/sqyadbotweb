509b7aa80bcd5e48d9aa50a895a199883c4cd9c6
Adderstorm <leonidp2@mail.ru>
2023-06-20 18:24:30 +0600

    Changed authorization method. Complete data transf

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import React from 'react';
 import './App.css';
 import DashBoard from './components/DashBoard/DashBoard';
 import MainPage from './components/MainPage/MainPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Servers from './components/MainPage/Servers/Servers';
 import About from './components/MainPage/About/About';
 import Home from './components/MainPage/HomePage/Home';
 import LoginPage from './components/DashBoard/LoginPage/LoginPage';
+import { OAuthContextProvider } from './OAuthProvider/OAuthProvider';
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-          <Routes>
-            <Route path='/' element={<MainPage/>}>
-              <Route index element={<Home/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/servers' element={<Servers/>}/>
-            </Route>
-            <Route path='/dashboard' element={<DashBoard/>}/>
-            <Route path='/loginpage' element={<LoginPage/>}/>
-          </Routes>
-      </div>
-    </BrowserRouter>
+    <Router>
+      <OAuthContextProvider>
+        <div>
+            <Routes>
+              <Route path='/' element={<MainPage/>}>
+                <Route index element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/servers' element={<Servers/>}/>
+              </Route>
+              <Route path='/dashboard' element={<DashBoard/>}/>
+              <Route path='/loginpage' element={<LoginPage/>}/>
+            </Routes>
+        </div>
+      </OAuthContextProvider>
+    </Router>
   );
 }
 
